cce3c6424ceddf605a9d59de1e1783089fb4b781
emarkov1 <markov.ewgenij@gmail.com>
2020-07-24 22:53:03 -0700

    remove logs

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
 
   state = initialState;
 
-  componentDidMount() {console.log('mount')
+  componentDidMount() {
     this.inter = setInterval(this.moveSnake, this.state.speed);
     document.onkeydown = this.onKeyDown;
   }
@@ -53,6 +53,7 @@ class App extends Component {
       case 39:
         this.setState({ direction: 'RIGHT' });
         break;
+      default:
     }
   }
 
@@ -73,6 +74,7 @@ class App extends Component {
       case 'UP':
         head = [head[0], head[1] - 2];
         break;
+      default:
     }
     dots.push(head);
     dots.shift();
@@ -93,7 +95,7 @@ class App extends Component {
     let head = snake[snake.length - 1];
     snake.pop();
     snake.forEach(dot => {
-      if (head[0] == dot[0] && head[1] == dot[1]) {
+      if (head[0] === dot[0] && head[1] === dot[1]) {
         this.onGameOver();
       }
     })
@@ -102,7 +104,7 @@ class App extends Component {
   checkIfEat() {
     let head = this.state.snakeDots[this.state.snakeDots.length - 1];
     let food = this.state.food;
-    if (head[0] == food[0] && head[1] == food[1]) {
+    if (head[0] === food[0] && head[1] === food[1]) {
       this.setState({
         food: getRandomCoordinates()
       })
@@ -120,8 +122,6 @@ class App extends Component {
   }
 
   increaseSpeed() {
-    console.log('change')
-    console.log(this.state.speed)
     if (this.state.speed > 10) {
       this.setState({
         speed: this.state.speed - 3
