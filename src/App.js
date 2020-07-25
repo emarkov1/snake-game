62edc26388efd420f1206fb6541bb0d8e60deec1
emarkov1 <markov.ewgenij@gmail.com>
2020-07-24 17:20:17 -0700

    added food for snake

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import Snake from './snake'
+import Food from './Food'
+
 
 class App extends Component {
+  state={
+    food:[6,8],
+    snakeDots: [
+      [0,0],
+      [2,0]
+    ]
+  }
   render() {
     return (
       <div className="game-area">
-        <div className="snake-dot" style={{ top: 0, left: 0 }}></div>
-        <div className="snake-dot" style={{ top: 0, left: '2%' }}></div>
-        <div className="snake-dot" style={{ top: 0, left: '4%' }}></div>
+        <Snake snakeDots={this.state.snakeDots}/>
+        <Food dot={this.state.food}/>
       </div>
     );
   }
